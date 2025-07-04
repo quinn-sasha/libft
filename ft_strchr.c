e1ac12b1660a1b5d0dd29c9a9f9d49c18185fb22
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-04 18:04:02 +0900

    Modified: cast char when comparing s[i] and c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 19:34:38 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/02 15:29:59 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/04 17:56:34 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)(s + i));
 		i++;
 	}
