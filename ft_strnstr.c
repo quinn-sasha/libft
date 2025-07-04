f3a74afdbb524d0c383ea3eb019e1272a1a73b1e
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-04 17:53:14 +0900

    Fixed: strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 20:16:09 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/02 16:12:23 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/04 17:52:43 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ char	*ft_strnstr(const char *str, const char *find, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (len <= 0)
-		return (NULL);
-	if (str[0] == '\0')
-		return (NULL);
 	if (find[0] == '\0')
 		return ((char *)(str));
+	if (str[0] == '\0')
+		return (NULL);
 	i = 0;
 	while (i < len && str[i])
 	{
@@ -31,6 +29,8 @@ char	*ft_strnstr(const char *str, const char *find, size_t len)
 		{
 			if (find[j] != str[i + j])
 				break ;
+			if (i + j >= len)
+				return (NULL);
 			j++;
 		}
 		if (find[j] == '\0')
