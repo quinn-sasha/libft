cc2cac3dde6a7ea16a809bf55e5b080bbeb807b8
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-04 18:15:00 +0900

    Fixed: cast char when comparing s and c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 16:05:57 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/02 16:06:00 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/04 18:13:08 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)(s + i));
 		i--;
 	}
