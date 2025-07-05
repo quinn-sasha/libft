4fa2c431194826801d27f801f17a4881f7d08e8d
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-05 13:03:49 +0900

    Modified: definiton of the space

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,28 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 17:04:45 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/04 19:42:30 by squinn           ###   ########.fr       */
+/*   Updated: 2025/07/05 11:43:56 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_isspace(int c)
+{
+	if (c == ' ')
+		return (1);
+	if (c == '\f')
+		return (1);
+	if (c == '\n')
+		return (1);
+	if (c == '\r')
+		return (1);
+	if (c == '\t')
+		return (1);
+	if (c == '\v')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -19,7 +36,7 @@ int	ft_atoi(const char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] == ' ')
+	while (ft_isspace(str[i]))
 		i++;
 	sign = 1;
 	if (str[i] == '+')
