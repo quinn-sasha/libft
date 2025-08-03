be0fb637be9572b5625d870c01a77b78704e929c
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-03 13:41:05 +0900

    Modified fuction protoypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 13:54:19 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/07 16:41:36 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/03 13:40:22 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strchr(const char *s, int c);
+int		ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *str, const char *find, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -46,7 +46,7 @@ int		ft_tolower(int c);
 void	*ft_calloc(size_t nmeb, size_t size);
 char	*ft_strdup(const char *s);
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, int start, int last);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
