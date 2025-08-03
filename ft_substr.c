bbe0acf45ce16f22f8cb536274faad54df675237
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-03 10:56:30 +0900

    Modified: ft_substr()

--- ft_substr.c
+++ ft_substr.c
@@ -6,30 +6,28 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 09:52:41 by squinn            #+#    #+#             */
-/*   Updated: 2025/07/02 16:47:56 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/03 10:55:53 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, int start, int last)
 {
-	size_t	src_len;
-	size_t	i;
+	int		src_len;
 	char	*new_string;
+	int		i;
 
-	if (len == 0)
-		return (ft_strdup(""));
 	src_len = ft_strlen(s);
 	if (start >= src_len)
 		return (ft_strdup(""));
-	if (len > src_len - start)
-		len = src_len - start;
-	new_string = malloc(len + 1);
+	if (start > last)
+		return (ft_strdup(""));
+	new_string = malloc(last - start + 2);
 	if (!new_string)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i + start <= last)
 	{
 		new_string[i] = s[i + start];
 		i++;
@@ -37,16 +35,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	new_string[i] = '\0';
 	return (new_string);
 }
-
-/*
-#include <stdio.h>
-
-int	main(int argc, char *argv[]) {
-	(void)argc;
-	char const *s = argv[1];
-	unsigned int start = atoi(argv[2]);
-	size_t len = atoi(argv[3]);
-	printf("Original string: %s\n", s);
-	printf("Substring: %s\n", ft_substr(s, start, len));
-}
-*/
